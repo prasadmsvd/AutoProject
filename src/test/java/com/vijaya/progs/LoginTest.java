e3933a7dbcfefaf62be7cab92aabeee3fb1828f0
msvdprasad <msvdprasad.369@gmail.com>
2022-01-30 15:32:52 +0530

    deepu progs automation pom update1

--- src/test/java/com/vijaya/progs/LoginTest.java
+++ src/test/java/com/vijaya/progs/LoginTest.java
@@ -2,16 +2,25 @@ package com.vijaya.progs;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
 
 	@Test
 	public void test1() throws InterruptedException
 	{
+		WebDriverManager.firefoxdriver().setup();
+
+		FirefoxOptions firefoxOptions = new FirefoxOptions();  
+		firefoxOptions.addArguments("--display=0");
+		WebDriver driver = new FirefoxDriver(firefoxOptions);
+		
 		 System.setProperty("webdriver.gecko.driver", ".\\jars\\geckodriver.exe");
-		 WebDriver driver = new FirefoxDriver();
+		// WebDriver driver = new FirefoxDriver();
 	    driver.navigate().to("http://www.google.com/");  
 	    Thread.sleep(2000);
         System.out.println(driver.getTitle());
